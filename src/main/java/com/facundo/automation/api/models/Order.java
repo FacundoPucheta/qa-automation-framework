daf3f51bb8c89c6402877730153a45e14be53cf4
FacundoPucheta <facundopucheta94@gmail.com>
2026-02-18 17:30:15 -0300

    feat(api): add data Models - Orders model - Pets model

---
+++ src/main/java/com/facundo/automation/api/models/Order.java
@@ -0,0 +1,57 @@
+package com.facundo.automation.api.models;
+
+public class Order {
+    private long id;
+    private long petId;
+    private String shipDate;
+    private String status;
+    private boolean complete;
+
+    public Order(long id, long petId, String shipDate, String status, boolean complete) {
+        this.id = id;
+        this.petId = petId;
+        this.shipDate = shipDate;
+        this.status = status;
+        this.complete = complete;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPetId() {
+        return petId;
+    }
+
+    public void setPetId(long petId) {
+        this.petId = petId;
+    }
+
+    public String getShipDate() {
+        return shipDate;
+    }
+
+    public void setShipDate(String shipDate) {
+        this.shipDate = shipDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+}
