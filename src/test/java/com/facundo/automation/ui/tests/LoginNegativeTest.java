0c43900773a199f9f5cb2fc6e53a2eae9795e20a
FacundoPucheta <facundopucheta94@gmail.com>
2026-02-18 12:23:20 -0300

    feat(ui): add LoginNegativeTest

---
+++ src/test/java/com/facundo/automation/ui/tests/LoginNegativeTest.java
@@ -0,0 +1,26 @@
+package com.facundo.automation.ui.tests;
+
+import com.facundo.automation.ui.base.BaseTest;
+import com.facundo.automation.ui.pages.HomePage;
+import com.facundo.automation.ui.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Tests unsuccessful login flow on Demoblaze.
+ * Verifies that an error alert is displayed when credentials are invalid.
+ */
+public class LoginNegativeTest extends BaseTest {
+    @Test
+    public void shouldShowErrorOnInvalidCredentials() {
+        new HomePage(driver).open();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openLoginModal();
+        loginPage.login(TEST_USERNAME, "wrongPassword");
+
+        String errorMsg = loginPage.getAlertMessage();
+        Assert.assertEquals(errorMsg, "Wrong password.",
+                "Error | Error message should be 'Wrong password.'");
+    }
+}
